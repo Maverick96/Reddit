7757ba584aba6931ce8a458f81c40e4a5ee8c7a4
Harsh Pancholi <harsh.pancholi@bridgesgi.com>
2017-11-27 15:24:02 +0530

    Displaying threads with image

--- js/index.js
+++ js/index.js
@@ -2,14 +2,36 @@ $(document).ready( () => {
     $.ajax( {url : "https://www.reddit.com/.json", success : onLoad})
 })
 
-var onLoad = (result)=> {
-    console.log(result)
-    createThread(result.data.children[0].data)
+var onLoad = (result) => {
+    //Iterate through the result of the ajax call
+    $.each(result.data.children, function(index,value){
+        createThread(value.data)
+    })
+    
 }
 
 let createThread = function(threadData){
-    $("#score").html(threadData.score)
-    $("#title").html(threadData.title)
-    $("#comments").html(threadData.num_comments)
-    $("#subredditName").html(threadData.subreddit)
+    //creating element for thread
+    try {
+        let $thread = $('<div/>',{'class' : 'thread'});
+        let $threadImage = $('<div/>', {'class' : 'thread-image'}).append($('<img/>',{'src' : threadData.preview.images[0].source.url, 'alt' : 'Thread Image', 'height' : '100','widht' : '100'}))
+        $thread.append($threadImage)
+        let $threadData = $('<div/>', {'class'  : 'thread-data'})
+        let $subredditName = $('<div/>', {'text' : "Subreddit : " + threadData.subreddit_name_prefixed})
+        $threadData.append($subredditName)
+        let $upVote = $('<div/>',{'text' : "Up Votes : " + threadData.ups})
+        $threadData.append($upVote)
+        let $link = $("<div/>",{'text' : "Source : " });
+        //hyperlink for the source
+        $("<a>",{'href' : threadData.url, 'text' : threadData.url, 'target' : '_blank'}).appendTo($link)
+        $threadData.append($link)
+        let $comments = $("<div/>", {'text' : "No. of Comments : " + threadData.num_comments})
+        $threadData.append($comments)
+        $thread.append($threadData)
+        $thread.append("<hr/>")
+        $("#content").append($thread)
+    }
+    catch(e){
+        console.log(e.message)
+    }
 }
